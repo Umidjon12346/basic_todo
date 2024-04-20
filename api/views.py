0b21524a327e220fbdf8edf7a2871f4795c6b330
Umidjon12346 <umidshari456@gmail.com>
2024-04-20 15:44:12 +0500

    serializer dn

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+from .models import Task
+from rest_framework.response import Response
+from rest_framework.request import Request
+from .serializer import TaskSerializer
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class TaskSet(APIView):
+    def get(self,request):
+        task = Task.objects.all()
+        d = []
+        for i in task:
+            d.append(TaskSerializer(i).data)
+        return Response(d)
