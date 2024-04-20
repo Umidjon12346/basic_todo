eb60aaaeea28e40e2ef674b7f2ec8d00606b9ba5
Umidjon12346 <umidshari456@gmail.com>
2024-04-20 15:30:08 +0500

    modelsdone

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
-# Create your models here.
+class Task(models.Model):
+    title       = models.CharField    (max_length= 200)
+    completed   = models.BooleanField (default=False)
+    discription = models.TextField    (blank=True,null=True)
+    created     = models.DateTimeField(auto_now_add = True)
+    user_id     = models.CharField    (max_length=200)
+    
+    def __str__(self) -> str:
+        return self.title    
